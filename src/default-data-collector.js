620e08047a9a4c3de282f35463430c4c8c8990d7
jakubroch <jakub.roch@cloudinary.com>
2024-05-29 11:02:42 +0200

    fix: add mobile browsers events

--- src/default-data-collector.js
+++ src/default-data-collector.js
@@ -3,28 +3,37 @@ import { prepareEvents } from './utils/prepare-events';
 export const setupDefaultDataCollector = (data, flushEvents, sendData, isMobile) => {
   const sendVideoData = () => {
     const collectedEvents = flushEvents();
-    const events = prepareEvents(collectedEvents);
-    sendData({
-      ...data,
-      events,
-    });
+
+    // multiple events can be triggered one by one for specific browsers but we don't have guarantee which ones
+    // in this case send data for first event and for rest just skip it to avoid empty payload
+    if (collectedEvents.length > 0) {
+      const events = prepareEvents(collectedEvents);
+      sendData({
+        ...data,
+        events,
+      });
+    }
   };
+
+  window.addEventListener('beforeunload', sendVideoData, {
+    once: true,
+  });
+
   const onMobileVisibilityChange = () => {
     if (document.visibilityState === 'hidden') {
       sendVideoData();
     }
   };
+  const onMobilePageHide = () => sendVideoData();
 
   if (isMobile) {
     document.addEventListener('visibilitychange', onMobileVisibilityChange);
-  } else {
-    window.addEventListener('beforeunload', sendVideoData, {
-      once: true,
-    });
+    window.addEventListener('pagehide', onMobilePageHide);
   }
 
   return () => {
     window.removeEventListener('beforeunload', sendVideoData);
+    window.addEventListener('pagehide', onMobilePageHide);
     document.removeEventListener('visibilitychange', onMobileVisibilityChange);
     sendVideoData();
   };
